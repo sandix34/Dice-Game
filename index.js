c01b4349e5f4e43cf17d6a9a5580a16f95685620
sandrine <sandrinezocca@gmail.com>
2021-03-13 18:39:42 +0100

    Change the player

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 // variables
 let randomNumber = 0;
 let roundScore = 0;
-
+let activePlayer = 0;
 
 // Get the #dice element
 const dice = document.querySelector("#dice");
 // Get roll button
 const roll = document.querySelector("#reload");
+// Get the players
+const player0 = document.querySelector(".player-0");
+const player1 = document.querySelector(".player-1");
 
-
-
-// Roll the dice
+// Roll the dice and display the round score
 const rollDice = function () {
-
   // Create a random number
   randomNumber = Math.floor(Math.random() * 6) + 1;
 
   // Display dice
-  dice.innerHTML =
-    '<img class="dice" src="./images/dice/dice-' +
-    randomNumber +
-    '.png" alt="dice ' +
-    randomNumber +
-    '">';
+  dice.innerHTML = `<img class="dice" src="./images/dice/dice-${randomNumber}.png" alt="dice ${randomNumber}">`;
 
   // Round score
-  if(randomNumber != 1) {
+  if (randomNumber !== 1) {
     roundScore += randomNumber;
-    console.log("roundScore" + roundScore);
+    // Display round score
+    document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
   } else {
-    roundScore = 0;
+    changePlayer();
   }
-  
-  // Display round score
-  document.querySelector('#current-0').textContent = roundScore;
 };
 
+// Change player
+const changePlayer = function () {
+  document.querySelector(`#current-${activePlayer}`).textContent = 0;
+  roundScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle("active-player");
+  player1.classList.toggle("active-player");
+};
 
 // Listen for click events
 roll.addEventListener("click", rollDice, false);
