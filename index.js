941a56172a1ab4cbc21251358792712ec1be7f56
sandrine <sandrinezocca@gmail.com>
2021-03-14 11:19:41 +0100

    addition of the round score to the overall score

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 let randomNumber = 0;
 let roundScore = 0;
 let activePlayer = 0;
+let scores = [0, 0];
 
 // Get the #dice element
 const dice = document.querySelector("#dice");
 // Get roll button
 const roll = document.querySelector("#reload");
+// Get hold button
+const hold = document.querySelector("#download");
 // Get the players
 const player0 = document.querySelector(".player-0");
 const player1 = document.querySelector(".player-1");
@@ -25,6 +28,7 @@ const rollDice = function () {
     // Display round score
     document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
   } else {
+    roundScore = 0;
     changePlayer();
   }
 };
@@ -32,11 +36,19 @@ const rollDice = function () {
 // Change player
 const changePlayer = function () {
   document.querySelector(`#current-${activePlayer}`).textContent = 0;
-  roundScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
   player0.classList.toggle("active-player");
   player1.classList.toggle("active-player");
 };
 
+// Hol the score
+const holdScore = function () {
+  // add current score
+  scores[activePlayer] += roundScore;
+
+  document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
+}
+
 // Listen for click events
 roll.addEventListener("click", rollDice, false);
+hold.addEventListener("click", holdScore, false);
